b3e5391f42209171a5fd36c69f9e0cbf53accac3
Josip <ujdur.jo@gmail.com>
2026-02-08 21:29:06 +0100

    gotov

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 /*
 Vježba 16:
 Kreirati novi Java projekt koji će sadržavati perzistentnu klasu „Account” s varijablama „id” (Long),
-„username” (String) i „balance” (BigDecimal) te odgovarajuću konfiguraciju u datoteci „hibernate.cfg.xml”.
+„username” (String) i „balance” (BigDecimal) te odgovarajuću konfiguraciju u datoteci „hibernate.cfg.xml”. ✓
+
 Kreirati novu klasu „HibernateUtil” koja će sadržavati privatnu metodu „buildSessionFactory” te javne
-metode „getSessionFactory” koja će vraćati kreirani „sessionFactory” objekt te metodu „shutdown” koja će zatvarati objekt „sessionFactory”.
+metode „getSessionFactory” koja će vraćati kreirani „sessionFactory” objekt te metodu „shutdown” koja će zatvarati objekt „sessionFactory”. ✓
 
 Kreirati klasu koja će sadržavati „main” metodu koja će imati metode za kreiranje objekta klase „Account”
 korištenjem sesije i transakcije, metodu za provođenje jedne transakcije koja prenosi sredstva s jednog računa na drugi te obavlja
-„commit” ako ima dovoljno sredstava na bankovnom računa, odnosno „rollback” ako nema dovoljno sredstava.
+„commit” ako ima dovoljno sredstava na bankovnom računa, odnosno „rollback” ako nema dovoljno sredstava.✓
 
-Na kraju napisati metodu za provjeru stanja zadanog računa koja također koristi Hibernate sesiju.
+Na kraju napisati metodu za provjeru stanja zadanog računa koja također koristi Hibernate sesiju.✓
 
 Primjer implementacije metode za kreiranje računa je prikazan u nastavku:
 public static Long createAccount(String username, BigDecimal initialBalance) {
@@ -43,11 +44,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-Long acc1 = createAccount("Otp", new BigDecimal("15000"));
-Long acc2 = createAccount("Josip", new BigDecimal("2340"));
-Long acc3 = createAccount("Biznis", new BigDecimal("79487"));
+        Long acc1 = createAccount("Otp", new BigDecimal("15000"));
+        Long acc2 = createAccount("Josip", new BigDecimal("2340"));
+        Long acc3 = createAccount("Biznis", new BigDecimal("79487"));
+
+        transferFunds("Otp", "Josip", BigDecimal.valueOf(14000));
+        transferFunds("Biznis", "Josip", BigDecimal.valueOf(80000));
 
 
+        chkBalance("Josip");
 
     }
 
@@ -55,61 +60,65 @@ Long acc3 = createAccount("Biznis", new BigDecimal("79487"));
     public static Long createAccount(String username, BigDecimal initialBalance) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
+
             Account account = new Account(username, initialBalance);
-            Long accountId = (Long) session.persist(account);
+            session.persist(account);
+            Long accountId = account.getId();
             transaction.commit();
             System.out.println("Account created with ID: " + accountId);
             return accountId;
         }
     }
 
-    // METODA 2: Transfer novca između računa
-    public static boolean transferFunds(String fromUser, String toUser, BigDecimal amount) {
+
+    public static boolean transferFunds(String senderUsername, String receiverUsername, BigDecimal amount) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = null;
 
         try {
             transaction = session.beginTransaction();
 
+
+            Account senderAccount = (Account) session.createQuery("FROM Account WHERE username = :username")
+                    .setParameter("username", senderUsername)
+                    .uniqueResult();
+
+
             Account receiverAccount = (Account) session.createQuery("FROM Account WHERE username = :username")
                     .setParameter("username", receiverUsername)
                     .uniqueResult();
-                    .createQuery("FROM Account WHERE username = :username")
-                    .setParameter("username", fromUser)
-                    .uniqueResult();
 
+            if (senderAccount == null || receiverAccount == null) {
+                System.out.println("Račun nije pronađen!");
+                if (transaction != null) transaction.rollback();
+                return false;
+            }
 
-
-            System.out.println("Pošiljatelj (" + sender.getUsername() + "): " + sender.getBalance());
-            System.out.println("Primatelj (" + receiver.getUsername() + "): " + receiver.getBalance());
+            System.out.println("Pošiljatelj: " + senderAccount.getUsername() + ", Stanje: " + senderAccount.getBalance());
+            System.out.println("Primatelj: " + receiverAccount.getUsername() + ", Stanje: " + receiverAccount.getBalance());
             System.out.println("Iznos za transfer: " + amount);
+            if (senderAccount.getBalance().compareTo(amount) >= 0) {
 
-            // Provjeri ima li dovoljno novca
-            if (sender.getBalance().compareTo(amount) >= 0) {
-                // Ima dovoljno - izvrši transfer
-                sender.setBalance(sender.getBalance().subtract(amount));
-                receiver.setBalance(receiver.getBalance().add(amount));
+                senderAccount.setBalance(senderAccount.getBalance().subtract(amount));
+                receiverAccount.setBalance(receiverAccount.getBalance().add(amount));
 
-                // Spremi promjene
-                session.update(sender);
-                session.update(receiver);
+                session.merge(senderAccount);
+                session.merge(receiverAccount);
 
                 transaction.commit();
                 System.out.println("Transfer uspješan!");
-                System.out.println("Novo stanje " + sender.getUsername() + ": " + sender.getBalance());
-                System.out.println("Novo stanje " + receiver.getUsername() + ": " + receiver.getBalance());
+                System.out.println("Nakon transfera:");
+                System.out.println("Pošiljatelj: " + senderAccount.getUsername() + ", Stanje: " + senderAccount.getBalance());
+                System.out.println("Primatelj: " + receiverAccount.getUsername() + ", Stanje: " + receiverAccount.getBalance());
                 return true;
-
             } else {
-                // Nema dovoljno novca - rollback
-                System.out.println("Nedovoljno sredstava na računu!");
-                System.out.println("Trenutno: " + sender.getBalance() + ", potrebno: " + amount);
+                System.out.println(senderAccount.getUsername() +" nema dovoljno sredstava na računu!");
                 transaction.rollback();
                 return false;
             }
 
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             System.out.println("Greška pri transferu: " + e.getMessage());
@@ -119,27 +128,30 @@ Long acc3 = createAccount("Biznis", new BigDecimal("79487"));
         }
     }
 
-    // METODA 3: Provjera stanja računa
-    public static void checkBalance(String username) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
 
-        try {
-            Account account = (Account) session
-                    .createQuery("FROM Account WHERE username = :username")
-                    .setParameter("username", username)
-                    .uniqueResult();
+            public static void chkBalance (String username){
+                Session session = HibernateUtil.getSessionFactory().openSession();
 
-            if (account != null) {
-                System.out.println(username + ": " + account.getBalance() + " kn");
-            } else {
-                System.out.println(username + ": Račun nije pronađen");
+                try {
+                    Account account = (Account) session
+                            .createQuery("FROM Account WHERE username = :username")
+                            .setParameter("username", username)
+                            .uniqueResult();
+
+                    if (account != null) {
+                        System.out.println(username + ": " + account.getBalance() + " eur");
+                    } else {
+                        System.out.println(username + ": Račun nije pronađen");
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Greška pri provjeri stanja: " + e.getMessage());
+                } finally {
+                    session.close();
+                }
             }
 
-        } catch (Exception e) {
-            System.out.println("Greška pri provjeri stanja: " + e.getMessage());
-        } finally {
-            session.close();
         }
-    }
-}
+
+
 
